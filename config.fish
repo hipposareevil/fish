a153ea8fc15c6891d3fa97fcdca23e3cfe0e4981
Sam Jackson <hipposareevil@gmail.com>
2021-12-31 17:05:02 -0800

    moving functions to my.functions

--- config.fish
+++ config.fish
@@ -11,13 +11,16 @@ fish_add_path $HOME/wpff/auto_backup
 
 # abbreviations
 abbr -a -g ag "ag --hidden"
-abbr -a -g commit "git commit --all -m \""
+abbr -a -g commit "git commit --all -m "
 abbr -a -g wpff "ssh web@willprogramforfood.com -p 66"
 abbr -a -g nextcloud "ssh hippo@hipposareevil.com"
 abbr -a -g epoch "perl -le 'print scalar localtime $argv[1]'"
 abbr -a -g rebase "git rebase -i HEAD~"
 abbr -a -g rebase.date "git rebase --ignore-date"
 
-
 # load custom functions, like git
 source ~/.config/fish/custom.fish
+
+# auto-load mine 
+set -g fish_function_path $fish_function_path ~/.config/fish/my.functions
+
